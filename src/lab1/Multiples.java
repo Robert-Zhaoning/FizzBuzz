00b0cc8494eb28c03ec527c3c50ed73e9ed5b03d
Zhaoning Zhang <robert.zhaoning@5e:83:8f:f6:36:0a.home>
2025-09-08 19:56:30 -0400

    Updated files

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,17 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        int i = 1;
-        int count = 0;
-        while(i<1000){
-           if( i%3==0 || i%5==0){
-               count++;
-           }
-            i++;
-        }
+        int count = multiples(1000,3,5);
         System.out.println(count);
+    }
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for(int i=1;i<n;i++){
+            if(i%a==0||i%b==0){
+                count++;
+            }
+
+        }
+        return count;
     }
 }
